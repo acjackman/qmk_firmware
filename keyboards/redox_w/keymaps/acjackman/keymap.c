9323d8f6dd8ccd18d552d195510235e925659917
Adam Jackman <adam@acjackman.com>
2020-12-31 02:29:34 -0700

    SFT_T was already defined

--- keyboards/redox_w/keymaps/acjackman/keymap.c
+++ keyboards/redox_w/keymaps/acjackman/keymap.c
@@ -44,7 +44,7 @@ enum custom_keycodes {
 #define SFT_LPRN LSFT(KC_9)
 #define SFT_RPRN LSFT(KC_0)
 #define SFT_CLN LSFT(KC_SCLN)
-#define SFT_T LSFT(KC_T)
+#define CAP_T LSFT(KC_T)
 
 #define M1_REC DYN_REC_START1
 #define M1_PLAY DYN_MACRO_PLAY1
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      _______ ,KC_HASH ,KC_DLR  ,KC_LBRC ,KC_RBRC ,KC_GRV  ,_______ ,                          _______ ,KC_PAST ,KC_P4 	,KC_P5   ,KC_P6   ,KC_PPLS ,KC_PAST ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     SFT_LPRN,KC_PERC ,KC_CIRC ,KC_LPRN ,KC_RPRN ,KC_TILD ,_______ ,_______ ,        _______ ,_______ ,SFT_CLN ,KC_P1 	,KC_P2   ,KC_P3   ,SFT_T   ,SFT_RPRN,
+     SFT_LPRN,KC_PERC ,KC_CIRC ,KC_LPRN ,KC_RPRN ,KC_TILD ,_______ ,_______ ,        _______ ,_______ ,SFT_CLN ,KC_P1 	,KC_P2   ,KC_P3   ,CAP_T   ,SFT_RPRN,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
      _______ ,_______ ,_______ ,_______ ,     _______ ,    _______ ,_______ ,        _______ ,_______ ,    KC_P0   ,     KC_P0   ,KC_PDOT ,KC_PENT ,KC_NLCK
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
